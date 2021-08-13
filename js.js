f6290b4e1d8f492b9c8ca26b848053b410e17e9a
bruno <henrikbruno852@gmail.com>
2021-08-12 21:23:10 -0400

    primo

---
+++ js.js
@@ -0,0 +1,25 @@
+function hour () {
+    var today = new Date();
+    var hour = today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
+
+    var clock = document.getElementById("clock");
+    clock.value = hour + ":" + minutes + ":" + (seconds<10 ? "0"+seconds : seconds);
+
+    setTimeout("hour()",1000);
+}
+
+hour();
+
+
+function fosco (){
+    var data = new Date();
+        var day = data.getDate();
+            var month = data.getMonth();
+                var year = data.getFullYear();
+                                                        
+            var tempo = document.getElementById("date");
+        tempo.value = day + "/" + (month+1) + "/" + year;
+}
+fosco();
